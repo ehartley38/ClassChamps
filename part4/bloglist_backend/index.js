3d101053064f53a37a6d8181303a36bd85be4f05
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-01-04 17:28:53 +0000

    Completed exercise 4.2 -- Restructured the application into separate modules

--- part4/bloglist_backend/index.js
+++ part4/bloglist_backend/index.js
@@ -1,46 +1,25 @@
-require('dotenv').config()
+const config = require('./utils/config')
 const http = require('http')
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const cors = require('cors')
-const mongoose = require('mongoose')
-const mongoUrl = process.env.MONGODB_URI
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-mongoose.connect(mongoUrl)
+const Blog = require('./models/blog')
+const logger = require ('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
 
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
+app.use(middleware.requestLogger)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = config.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
