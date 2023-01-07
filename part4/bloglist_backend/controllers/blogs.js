15d18cb8a08fb429c12029d78c85d1960a48231f
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-01-07 18:30:57 +0000

    Added routes for put and delete for /api/blogs/id. Added test for delete.

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -6,11 +6,40 @@ blogsRouter.get('/', async (request, response) => {
         response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response) => {
+    const id = request.params.id
+    const blog = await Blog.findById(id).exec()
+    response.json(blog)
+})
+
 blogsRouter.post('/', async (request, response) => {
     const blog = new Blog(request.body)
     const result = await blog.save()
 
     response.status(201).json(result)
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+    const body = request.body
+
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+        request.params.id,
+        blog,
+        { new: true }
+    );
+    response.json(updatedBlog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+    await Blog.findByIdAndRemove(request.params.id)
+    response.status(204).end()
 })
 
 module.exports = blogsRouter
--- part4/bloglist_backend/tests/blog_api.test.js
+++ part4/bloglist_backend/tests/blog_api.test.js
@@ -15,38 +15,54 @@ beforeEach(async () => {
     await Promise.all(promiseArray)
 })
 
-test('all blogs are returned', async () => {
-    try {
-        const response = await api
-            .get('/api/blogs')
+describe('when there is initially some blogs saved', () => {
+    test('all blogs are returned', async () => {
+        try {
+            const response = await api
+                .get('/api/blogs')
+                .expect('Content-Type', /application\/json/)
+    
+            expect(response.body).toHaveLength(helper.initialBlogs.length)
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+    test('deleting a blog is successful', async () => {
+        const idToDelete = '5a422a851b54a676234d17f7'
+        await api
+            .delete(`/api/blogs/${idToDelete}`)
+            .expect(204)
+        
+        const deletedBlog = await Blog.findById(idToDelete)
+        expect(deletedBlog).toBeNull()
+    })
+})
+
+
+describe('addition of a new blog', () => {
+    test('succeeds with valid data', async () => {
+        const newBlog = {
+            "title": "testing",
+            "author": "Ed",
+            "url": "www.test.com",
+            "likes": 1
+        }
+    
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
             .expect('Content-Type', /application\/json/)
-
-        expect(response.body).toHaveLength(helper.initialBlogs.length)
-    } catch (error) {
-        console.error(error)
-    }
+    
+        const blogsAtEnd = await helper.blogsInDb()
+        expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+    
+        const titles = blogsAtEnd.map(b => b.title)
+        expect(titles).toContain("testing")
+    })
 })
 
-test('a valid blog can be added', async () => {
-    const newBlog = {
-        "title": "testing",
-        "author": "Ed",
-        "url": "www.test.com",
-        "likes": 1
-    }
-
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    const blogsAtEnd = await helper.blogsInDb()
-    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-    const titles = blogsAtEnd.map(b => b.title)
-    expect(titles).toContain("testing")
-})
 
 afterAll(() => {
     mongoose.connection.close()
