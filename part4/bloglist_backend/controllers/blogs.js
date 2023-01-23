5282762a911c32e3425eebea2f9d26b7071d485a
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-01-23 18:06:54 +0000

    Added token based authentication so that its only possible to add new blogs if a valid token is sent with the POST request

--- part4/bloglist_backend/controllers/blogs.js
+++ part4/bloglist_backend/controllers/blogs.js
@@ -1,12 +1,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+const config = require('../utils/config')
+
+
+const getTokenFrom = request => {  
+    const authorization = request.get('authorization')  
+    if (authorization && authorization.startsWith('bearer ')) {
+        return authorization.replace('bearer ', '')  
+    }  
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
-        const blogs = await Blog
-        .find({}).populate('user', {username: 1, name: 1})
-        
-        response.json(blogs)
+    const blogs = await Blog
+        .find({}).populate('user', { username: 1, name: 1 })
+
+    response.json(blogs)
 })
 
 blogsRouter.get('/:id', async (request, response) => {
@@ -17,8 +28,14 @@ blogsRouter.get('/:id', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+    console.log(`Secret is ${config.SECRET}`);
+    console.log(`Token decoded is ${getTokenFrom(request)}`);
+    const decodedToken = jwt.verify(getTokenFrom(request), config.SECRET)
 
-    const user = await User.findById(body.userId)
+    if (!decodedToken.id) {    
+        return response.status(401).json({ error: 'token invalid' })  
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
