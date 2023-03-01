dc2d526c00e52fb557b11aba7cba03ec601c999e
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-03-01 17:28:44 +0000

    Added template for adding questions, answers and hints for bingo

--- frontend/src/components/teacher/homework/AddQuestions.js
+++ frontend/src/components/teacher/homework/AddQuestions.js
@@ -1,10 +1,62 @@
+import { Box, TextField, Grid, Button } from "@mui/material"
+import { useState } from "react"
+
 export const AddQuestions = ({ homeworkType }) => {
+    const [questions, setQuestions] = useState([])
 
 
-    
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('submit');
+    }
+
     return (
         <>
             <h1>Add {homeworkType} questions</h1>
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <form onSubmit={handleSubmit}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="question"
+                            label="Question"
+                            id="question"
+                            autoComplete="Question"
+                        >
+                        </TextField>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="answer"
+                            label="Answer"
+                            id="answer"
+                            autoComplete="Answer"
+                        >
+
+                        </TextField>
+                        <TextField
+                            margin="normal"
+                            fullWidth
+                            name="hints"
+                            label="Hints"
+                            id="hints"
+                            autoComplete="Hints"
+                        >
+
+                        </TextField>
+                        <Button
+                            type="submit"
+                            variant="contained"
+                        >
+                            Add
+                        </Button>
+                    </form>
+
+                </Grid>
+            </Grid>
         </>
     )
 }
