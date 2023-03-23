b1cf1d89ce07b05316ee4a46eb15a6c9afa83f38
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-03-23 13:05:05 +0000

    Added progress bar with styling to user profile page

--- frontend/src/components/student/Profile.js
+++ frontend/src/components/student/Profile.js
@@ -1,7 +1,8 @@
-import { Grid, Typography } from "@mui/material"
+import { Box, Grid, Typography } from "@mui/material"
 import useAuth from "../../providers/useAuth"
 import { calculateLevel } from "../../utils/tools"
 import ProgressBar from "@ramonak/react-progress-bar";
+import CountUp from 'react-countup'
 
 export const Profile = () => {
     const { user, jwt } = useAuth()
@@ -11,27 +12,38 @@ export const Profile = () => {
 
     return (
         <>
-            <Typography variant="h3" sx={{ mt: 3 }}>
-                {`${user.name}'s Profile`}
-            </Typography>
-            {user.experiencePoints} XP
-            Level {level}
             <Grid
                 container
                 spacing={0}
                 alignItems="center"
                 justifyContent="center"
             >
-                <Grid item xs={10}>
-                    <ProgressBar
-                        completed={percentage}
-                        labelAlignment="center"
-                        labelColor="#ffffff"
-                        transitionTimingFunction="ease"
-                        animateOnRender
-                    />
+                <Grid item xs={8}>
+                    <Box className="circle" sx={{ mt:5, mb:-2 }}>
+                        <span className="number">{level}</span>
+                    </Box>
+                    <Box sx={{ textAlign: "center", mt: 0}}>
+                        <CountUp end={user.experiencePoints} duration={1} /> XP
+                    </Box>
+                    <Box sx={{ mt: 1 }}>
+                        <ProgressBar
+                            completed={percentage}
+                            labelAlignment="center"
+                            labelColor="#ffffff"
+                            transitionTimingFunction="ease"
+                            animateOnRender
+                            isLabelVisible={false}
+                        />
+                        <Box style={{ display: "flex", justifyContent: "space-between" }}>
+                            <div>{`Level ${level}`}</div>
+                            <div>{`Level ${level + 1}`}</div>
+                        </Box>
+                    </Box>
+
+
                 </Grid>
             </Grid>
+
 
         </>
     )
