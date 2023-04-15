bcbed34ad9990450bca2eeae665e2e507a6c450f
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-04-15 19:26:45 +0100

    Committing production build

--- backend/app.js
+++ backend/app.js
@@ -42,6 +42,7 @@ app.use(middleware.credentials);
 app.use(cors(corsOptions));
 
 app.use(express.json());
+app.use(express.static("build"));
 app.use(morgan("tiny"));
 app.use(middleware.requestLogger);
 app.use(cookieParser());
