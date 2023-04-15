785f915ad98eb67eb1a76ffa10c2bc29fc01d46f
Ed Hartley (UG) <b9012949@newcastle.ac.uk>
2023-04-15 15:53:04 +0100

    Removed legacy code

--- frontend/src/services/badges.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-const baseUrl = "/api/badges";
-
-const getAll = async () => {
-  const response = await axios.get(baseUrl);
-  return response.data;
-};
-
-export default { getAll };
